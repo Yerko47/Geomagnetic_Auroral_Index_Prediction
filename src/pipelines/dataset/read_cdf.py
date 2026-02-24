b5ae0b044efbdcc16694c658558b9d89e6ec676a
Yerko47 <yerko.jelcic@usach.cl>
2026-02-23 21:46:47 -0300

    Corrección de errores en el código y mejoramiento de la lógica

--- src/pipelines/dataset/read_cdf.py
+++ src/pipelines/dataset/read_cdf.py
@@ -15,12 +15,55 @@ def dataset(config: dict, paths: dict) -> pd.DataFrame:
     Returns:
         - df (pd.DataFrame): DataFrame con los datos de los archivos CDF procesados, limpios y con los tipos de datos correspondientes.
     """
+
+    def cdf_read(cdf_file_path: Path) -> pd.DataFrame:
+        """
+        Función encargada de leer el archivo CDF y convertirlo a un DataFrame. Además, se encarga de convertir la columna "Epoch" a formato datetime y renombrar las columnas 'E' a 'E_Field' y 'F' a 'B_Total'
+        """
+        try:
+            cdf = cdflib.CDF(cdf_file_path)
+        except FileNotFoundError or Exception as e:
+            print(f"Error al abrir o leer el archivo CDF {e}")
+
+        cdf_dict = {}
+        
+        for key in cdf.cdf_info().zVariables:
+            cdf_dict[key] = cdf[key][...]
+
+        cdf_df = pd.DataFrame(cdf_dict)
+        cdf_df["Epoch"] = pd.to_datetime(cdflib.cdfepoch.encode(cdf_df["Epoch"].values))
+        cdf_df = cdf_df.rename(columns={"E": "E_field", "F": "B_Total"})
+
+        return cdf_df
+    
+    def bad_data(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Función encargada de procesar los datos erroneos del DataFrame entregado. Para cada columna, se establece un umbral máximo basado en el valor máximo de la columna, redondeado hacia abajo a dos decimales. Los valores que excedan este umbral se reemplazan por NaN. Luego, se realiza una interpolación lineal para llenar los valores faltantes, seguido de un backfill y un forwardfill con un límite de 2 para cada uno. Finalmente, devuelve el DataFrame limpiado.
+        """
+        if df.empty: return df
+
+        processed_df = df.copy()
+
+        for col in df.columns:
+            if col == "Epoch": continue
+
+            max_threshold = np.floor(processed_df[col].max() * 100) / 100
+            df.loc[df[col] >= max_threshold, col] = np.nan
+
+            df[col] = df[col].interpolate(method = "linear")
+            df[col] = df[col].bfill(limit = 3)
+            df[col] = df[col].ffill(limit = 3)
+
+        return df
+        
+
     omni_path = Path("data/omni/hro_1min")
     
     start_time = pd.Timestamp(config["dataset"]["time_range"]["start"])
     end_time = pd.Timestamp(config["dataset"]["time_range"]["end"])
 
     save_feather_file = paths["raw_file"] / Path(f"data_{start_time.year}_to_{end_time.year}.feather")
+    print(save_feather_file)
     
     if not save_feather_file.exists():
         print(f"\n Procesando OMNI data desde la fecha {start_time.strftime('%Y-%m-%d')} a {end_time.strftime('%Y-%m-%d')}\n"
@@ -31,7 +74,7 @@ def dataset(config: dict, paths: dict) -> pd.DataFrame:
         date_frame = []
 
         for date in date_array:
-            cdf_file_name = f"omni_hro_1min_{date.strftime('%Y%m011')}_v01.cdf"
+            cdf_file_name = f"omni_hro_1min_{date.strftime('%Y%m01')}_v01.cdf"
             cdf_file_path = omni_path / str(date.year) / cdf_file_name
             if cdf_file_path.exists():
                 cdf_df = cdf_read(cdf_file_path)
@@ -52,50 +95,17 @@ def dataset(config: dict, paths: dict) -> pd.DataFrame:
         return df
     
     elif save_feather_file.exists():
+        
         try:
             print(f" Cargando los datos procesados desde el archivo feather en {save_feather_file}")
-            return pd.read_feather(save_feather_file)
+            df = pd.read_feather(save_feather_file)
+            df = bad_data(df)
+            df = df[[col for col in ["Epoch"] + config["dataset"]["omni_variables"] + config["dataset"]["auroral_variables"]]]
+            
+            return df
+        
         except FileNotFoundError:
             print(f"El archivo feather no existe en {save_feather_file} y no se pudieron procesar los archivos CDF")
 
 
-    def cdf_read(cdf_file_path: Path) -> pd.DataFrame:
-        """
-        Función encargada de leer el archivo CDF y convertirlo a un DataFrame. Además, se encarga de convertir la columna "Epoch" a formato datetime y renombrar las columnas 'E' a 'E_Field' y 'F' a 'B_Total'
-        """
-        try:
-            cdf = cdflib.CDF(cdf_file_path)
-        except FileNotFoundError or Exception as e:
-            print(f"Error al abrir o leer el archivo CDF {e}")
-
-        cdf_dict = {}
-        
-        for key in cdf.cdf_info().zVariables:
-            cdf_dict[key] = cdf[key][...]
-
-        cdf_df = pd.DataFrame(cdf_dict)
-        cdf_df["Epoch"] = pd.to_datetime(cdflib.cdfepoch.encode(cdf_df["Epoch"].values))
-        cdf_df.rename(columns = {"E": "E_field", "F": "B_Total"})
-
-        return cdf_df
     
-    def bad_data(df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Función encargada de procesar los datos erroneos del DataFrame entregado. Para cada columna, se establece un umbral máximo basado en el valor máximo de la columna, redondeado hacia abajo a dos decimales. Los valores que excedan este umbral se reemplazan por NaN. Luego, se realiza una interpolación lineal para llenar los valores faltantes, seguido de un backfill y un forwardfill con un límite de 2 para cada uno. Finalmente, devuelve el DataFrame limpiado.
-        """
-        if df.empty: return df
-
-        processed_df = df.copy()
-
-        for col in df.columns:
-            if col == "Epoch": continue
-
-            max_threshold = np.floor(processed_df[col].max() * 100) / 100
-            df.loc[col >= max_threshold, col] = np.nan
-
-            df[col] = df[col].interpolate(method = "linear")
-            df[col] = df[col].bfill(limit = 2)
-            df[col] = df[col].ffill(limit = 2)
-
-            return df
-        
