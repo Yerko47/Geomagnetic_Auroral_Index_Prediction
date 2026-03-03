4ccb137801eb53731e8714b80fda1b96c6ad71f4
Yerko47 <yerko.jelcic@usach.cl>
2026-03-03 19:31:03 -0300

    Se mejoraron las variables en el archivo YAML de acuerdo a las necesidades que se han creado en el código. También se agregó la clase OMNIDataset al archivo __init__ y se aplicó en el archivo main.py para crear los tres conjuntos necesarios

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import torch
 import pandas as pd
 import numpy as np
 
+import time
+
 def main():
 
     overrides = config_overrides()
@@ -15,7 +17,7 @@ def main():
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     seed = set_seed(42)
-    
+
     ts = pd.Timestamp(config["dataset"]["time_range"]["end"]).year
 
     omni = config["dataset"]["omni_variables"]
@@ -29,6 +31,25 @@ def main():
     
 
     df, scaler = scaler_fit(df = df, config = config)
+
+    delay_length = config["hyparameter"]["constant"]["delay_length"]
+
+    for delay in delay_length:
+
+        datatorch_train = OMNIDataset(df, config, delay = delay, split = "train")
+        datatorch_valid = OMNIDataset(df, config, delay = delay, split = "valid")
+        datatorch_test = OMNIDataset(df, config, delay = delay, split = "test")
+    
+
+    
+
+    #train_torch = OMNI_Dataset(df = df, config = config, delay = 30, split = "train")
+    #val_torch = OMNI_Dataset(df = df, config = config, delay = 30, split = "val")
+    #test = OMNI_Dataset(df = df, config = config, delay = 30, split = "test")
+    #print(test.X.shape)
+    #print(test.epoch)
+    #test_torch, test_epoch = (test.X, test.y), test.epoch
+
 
 
 
--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -1,9 +1,10 @@
 from .dataset.read_cdf import dataset
-from .dataset.temporal_preprocessing import storm_selection, scaler_fit
+from .dataset.temporal_preprocessing import storm_selection, scaler_fit, OMNIDataset
 
 
 __all__ = [
     "dataset",
     "storm_selection",
     "scaler_fit",
+    "OMNIDataset"
 ]
